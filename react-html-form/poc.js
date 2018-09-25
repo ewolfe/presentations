118d934d5ecb714d14d074e4e845284741579081
ewolfe <e@ewolfe.me>
2018-09-24 22:07:28 -0700

    docs: add second half of presentation

---
+++ react-html-form/poc.js
@@ -0,0 +1,23 @@
+class ProofOfConcept extends React.Component {
+  handleChange() {
+    let values = {};
+    for (let i = 0; i < this.form.elements.length; i += 1) {
+      const element = this.form.elements[i];
+      values[element.name] = element.value;
+    }
+    // do something with `values`
+  }
+
+  render() {
+    return (
+      <form
+        onChange={this.handleChange}
+        ref={element => {
+          this.form = element;
+        }}
+      >
+        {this.props.children}
+      </form>
+    );
+  }
+}
